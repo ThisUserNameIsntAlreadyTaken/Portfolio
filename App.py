447ed9fdef4b21ad960860ff0cb340233d0cb97c
ThisUserNameIsntAlreadyTaken <110028995+ThisUserNameIsntAlreadyTaken@users.noreply.github.com>
2022-07-26 18:33:58 +0800

    Add files via upload
    Hello World - Created on 26 July 2022

---
+++ App.py
@@ -0,0 +1,25 @@
+i = 1
+
+while i <= 5:
+    print (i * 'X')
+    i += 1
+
+
+#Weight = float(input("Enter weight: "))
+#Select = input("Convert to (K)ilograms or (P)ounds: ")
+
+#if Select.upper() == "K":
+    #Conversion = Weight * 0.45
+    #print ("Weight in Kg: " + str(Conversion))
+#elif Select.upper() == "P":
+    #Conversion = Weight / 0.45
+   # print ("Weight in Lb: " + str (Conversion))
+
+
+
+
+
+
+
+
+
